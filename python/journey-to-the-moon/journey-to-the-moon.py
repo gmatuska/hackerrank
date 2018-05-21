b48297d910ad4954dab6da26d128fcaa54e95059
gmatuska <garrett.matuska@gmail.com>
2018-05-20 23:24:08 -0700

    added dfs to graph and main function

--- python/journey-to-the-moon/journey-to-the-moon.py
+++ python/journey-to-the-moon/journey-to-the-moon.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import os
+import math
 
 
 class Graph:
@@ -10,11 +10,49 @@ class Graph:
     def add_edge(self, edge):
         self._edgeList.append(edge)
 
+    @staticmethod
+    def vertices():
+        return []
+
+    def dfs(self):
+        counts = []
+        visited = []
+        for v in self.vertices():
+            if filter(lambda x: x == v.value, visited): continue
+            result = []  # self.dfs_inside(v, DfsResult(visited, 0))
+            visited = False  # result.IsVisited()
+            counts.append(len(result))
+
+    @staticmethod
+    def dfs_inside(vertex, result):
+        stack = [vertex.value()]
+        while len(stack) > 0:
+            stackVertex = stack.pop()
+            if filter(lambda v: v != stackVertex, result.visited()):
+                result.visited().append(stackVertex)
+                # len(result)
+
+
+class DfsResult:
+
+    def __init__(self, visited, count):
+        self._visited = visited
+        self._count = count
+
+    def visited(self):
+        return self._visited
+
+    def count(self):
+        return self._count
+
 
 class Vertex:
 
     def __init__(self, value):
         self._value = value
+
+    def value(self):
+        return self._value
 
 
 class Edge:
@@ -25,9 +63,20 @@ class Edge:
 
 if __name__ == '__main__':
     tokens = input().strip().split(' ')
-    n = int(os.sys.argv[0])
-    m = int(os.sys.argv[1])
+    n = int(tokens[0])
+    m = int(tokens[1])
     graph = Graph()
     for _ in range(m):
         a, b = input().strip().split(' ')
         graph.add_edge(Edge(Vertex(int(a)), Vertex(int(b))))
+    singletons = graph.vertices()
+    results = []  # graph.dfs()
+    combinations = 0
+    for p in range(len(results)):
+        for q in range(p+1, len(results)):
+            combinations += (results[p] * results[q])
+    combinations += math.fsum(list(filter(lambda t: t * singletons, results)))
+    """connect disjoint sets to themselves and aggregate combinations"""
+    for p in range(len(singletons)-1, 0, -1):
+        combinations += p
+    print(combinations)
---
+++ python/journey-to-the-moon/test-journey-to-the-moon.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from unittest import TestCase
+
+
+class TestVertices(TestCase):
+
+    def test_using_lambda_in_vertices(self):
+        vertices = [1, 2, 3, 4, 5]
+        coll = list(filter(lambda x: x % 2 == 0, vertices))
+        self.assertEqual(2, len(coll))
